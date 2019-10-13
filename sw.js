7ed18b00bb90f6f6a148ce21ff7137a41da5fc9f
Jinyoung Park <jypark04070@gmail.com>
2019-10-13 16:44:01 +0900

    Fix the issue with mobile responsiveness

--- sw.js
+++ sw.js
@@ -6,6 +6,13 @@ self.addEventListener("install", async event => {
 
 self.addEventListener("fetch", async event => {
     console.log('fetch event')
+    const req = event.request;
+
+    if  (/.*(json)$.test(req.url)) {
+        event.respondWith(networkFirst(req));
+    } else {
+        event.respondWith(cacheFirst(req));
+    }
 });
 
 const cacheName = 'pwa-conf-v1';
@@ -15,3 +22,21 @@ const staticAssets = [
     './app.js',
     './style.css'
 ];
+
+async function cacheFirst(req) {
+    const cache = await caches.open(cacheName);
+    const cachedResponse = await cache.match(req);
+    return cachedResponse || networkFirst(req);
+}
+
+async function networkFirst(req) {
+    const cache = await caches.open(cacheName);
+    try {
+        const fresh = await fetch(req);
+        cache.put(req, fresh.clone());
+        return fresh;
+    } catch (e) {
+        const cachedResponse = await cache.match(req);
+        return cachedResponse;
+    }
+}
